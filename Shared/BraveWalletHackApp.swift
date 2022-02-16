321409fc3f4bba887245a7b3a848164858a85faa
grayson-bc <grayson@blockchain.com>
2022-02-16 21:49:23 +0000

    Initial Commit

---
+++ Shared/BraveWalletHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraveWalletHackApp.swift
+//  Shared
+//
+//  Created by Grayson Ho on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BraveWalletHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
